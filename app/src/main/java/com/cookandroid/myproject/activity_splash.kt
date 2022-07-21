3268c26b68dda172872a1968a1c8d2c91ce3905d
yeon <riyeon35@gmail.com>
2022-07-21 16:16:31 +0900

    LOADING

---
+++ app/src/main/java/com/cookandroid/myproject/activity_splash.kt
@@ -0,0 +1,37 @@
+package com.cookandroid.myproject
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.cookandroid.myproject.databinding.ActivitySplashBinding
+import android.content.Intent
+
+import android.os.Handler
+class activity_splash : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
