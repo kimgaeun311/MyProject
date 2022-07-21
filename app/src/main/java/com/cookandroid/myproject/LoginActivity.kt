21f038ce5c6417328906c689f886766c77d5e61e
kimgaeun <krlarkdms@gmail.com>
2022-07-21 22:18:53 +0900

    test

--- app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
@@ -2,12 +2,15 @@ package com.cookandroid.myproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setVisible(false)
+        title = "로그인"
 
-        title = "타이틀 "
+
     }
 }
--- app/src/main/java/com/cookandroid/myproject/activity_splash.kt
+++ app/src/main/java/com/cookandroid/myproject/activity_splash.kt
@@ -18,6 +18,7 @@ class activity_splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setVisible(false)
         title = "좀좀 일기"
         Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
