c9c97fb0f18eeb39b151ab778745ebae126916fa
asus <krlarkdms@gmail.com>
2022-07-27 00:49:49 +0900

    login

--- app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 
 class LoginActivity : AppCompatActivity() {
     lateinit var login_button:Button
 
+    lateinit var edtID : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setVisible(false)
+        edtID = findViewById(R.id.edtID)
+
+
         title = "로그인"
 
         login_button=findViewById(R.id.login_button)
         login_button.setOnClickListener {
+
+
+            Toast.makeText(this, edtID.text.toString()+ "님 어서오세요!", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
