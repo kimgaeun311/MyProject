89f7785234891ba53036319c924a525953dc0637
yeon <riyeon35@gmail.com>
2022-07-27 00:13:54 +0900

    day123

---
+++ app/src/main/java/com/cookandroid/myproject/dayClickActivity.kt
@@ -0,0 +1,26 @@
+package com.cookandroid.myproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class dayClickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_click_day)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+}
