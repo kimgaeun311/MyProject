d267d5022701464d9d156069a7d8f7399ac1e4e9
Aaamari <kengjin_@naver.com>
2022-07-26 22:08:14 +0900

    loginintent move

--- app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/myproject/LoginActivity.kt
@@ -1,16 +1,25 @@
 package com.cookandroid.myproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.ActionBar
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var login_button:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setVisible(false)
         title = "로그인"
 
+        login_button=findViewById(R.id.login_button)
+        login_button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
