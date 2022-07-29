5bd8ad8c6a61d3b1abadbd892ce93568279dc0d3
asus <krlarkdms@gmail.com>
2022-07-29 10:04:03 +0900

    login

--- app/src/main/java/com/cookandroid/myproject/dayClickActivity.kt
+++ app/src/main/java/com/cookandroid/myproject/dayClickActivity.kt
@@ -3,6 +3,8 @@ package com.cookandroid.myproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class dayClickActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,19 @@ class dayClickActivity : AppCompatActivity() {
         setContentView(R.layout.activity_click_day)
 
        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val spinner1: Spinner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.my_list,
+            R.layout.spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner1.adapter = adapter
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
